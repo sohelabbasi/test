2ccd2d43a06ff4fce470bcd2bf9d30bfdfbbef47
sohai <SyntaxTechnologies>
2022-12-30 10:35:57 +0530

    lc after jenkins message

--- src/test/java/runners/RunnerClass.java
+++ src/test/java/runners/RunnerClass.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
         //and it quickly scans all the steps whether the step defs are implemented or not
         //if step def is missing, it will provide step def for particular step
         dryRun = false,
+        //this tag i am adding to check the scenario for CI/CD pipeline
         tags = "@cmd",
         plugin = {"pretty", "html:target/cucumber.html","json:target/cucumber.json",
         "rerun:target/failed.txt"}
